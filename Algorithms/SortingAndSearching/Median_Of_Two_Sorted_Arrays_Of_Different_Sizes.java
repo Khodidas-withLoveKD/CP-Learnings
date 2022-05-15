5df3ad64afd30f5d863831cf79f9212f871d9872
Khodidas_with_love_KD <chauhan.khodidas@gmail.com>
2022-05-16 01:54:13 +0530

    Median_Of_Two_Sorted_Arrays_Of_Different_Sizes Try#2 incomplete

--- Algorithms/SortingAndSearching/Median_Of_Two_Sorted_Arrays_Of_Different_Sizes.java
+++ Algorithms/SortingAndSearching/Median_Of_Two_Sorted_Arrays_Of_Different_Sizes.java
@@ -59,68 +59,131 @@ public class Median_Of_Two_Sorted_Arrays_Of_Different_Sizes {
       The array size will reduce to min 2
       Let's code!!
     */
-    int median = -1;
+    float median = -1;
 
     int l1 = 0, h1 = n - 1,
             l2 = 0, h2 = m - 1;
 
-    while (h1 - l1 > 2 && h2 - l2 > 2) {
-      int size1 = h1 - l1 + 1;
-      int size2 = h2 - l2 + 1;
-      float m1 = getMedian(arr1, l1, h1, size1);
-      float m2 = getMedian(arr2, l2, h2, size2);
+    int size1 = n,
+            size2 = m;
 
-      int medianIndex1 = size1/2 - 1;
-      int medianIndex2 = size2/2 - 1;
+    while (size1 > 2 && size2 > 2) {
+      float m1 = getMedian(arr1, l1, h1);
+      float m2 = getMedian(arr2, l2, h2);
+
+      int medianIndex1 = size1/2;
+      int medianIndex2 = size2/2;
       // if m1 < m2
       if (m1 < m2) {
-        // discared firt half of arr1
-        l1 = l1 + medianIndex1;
+        // discard first half of arr1
+        l1 = l1 + medianIndex1 + ((size1 % 2 == 0) ? -1 : 0);
         // discard second half of arr2
-        h2 = (size2 % 2 == 0) ? l2 + medianIndex2 + 1 : l2 + medianIndex2;
+        h2 = l2 + medianIndex2;
       } else {
         // discard the first half of arr2
-        l2 = l2 + medianIndex2;
-        h1 = (size1 % 2 == 0) ? l1 + medianIndex1 + 1 : l1 + medianIndex1;
+        l2 = l2 + medianIndex2 + ((size2 % 2 == 0) ? -1 : 0);
+        h1 = l1 + medianIndex1;
       }
+      size1 = h1 - l1 + 1;
+      size2 = h2 - l2 + 1;
     }
     // breaks when one / both arr <= 2
 
-    // case 1: sm = 0, l = ?
-    int l, h;
 
-    if (h1 - l1 == 0 || h2 - l2 == 0) {
+    // case 1: sm = 0, l = ?
+    if (size1 == 0 || size2 == 0) {
       // med = median of large arr
       if (h1 - l1 != 0) {
-        int size1 = h1 - l1 + 1;
         median = (size1 % 2 == 0) ?
-                (arr1[size1/2 - 1] + arr1[size1/2])
-                : arr1[size1/2];
+                (arr1[l1 + size1/2 - 1] + arr1[l1 + size1/2])
+                : arr1[l1 + size1/2];
       } else {
-        int size2 = h2 - l2 + 1;
         median = (size2 % 2 == 0) ?
-                (arr1[size2/2 - 1] + arr1[size2/2])
-                : arr1[size2/2];
+                (arr2[l2 + size2/2 - 1] + arr2[l2 + size2/2])
+                : arr2[l2 + size2/2];
       }
     }
 
     // case 2: sm = 1
-    // 2.1 l = 1
-    if (h1 - l1 == 1 && h2 - l2 == 1) {
-      // med of both
-      median = (arr1[0] + arr2[0]) / 2;
+    if (size1 == 1 || size2 == 1) {
+      // 2.1 l = 1
+      if (size1 == 1 && size2 == 1) {
+        // med of both
+        median = (float)((arr1[l1] + arr2[l2]) /2.0);
+      }
+
+      // 2.2 l = even
+      // median of three
+      else if (size2 % 2 == 0) {
+        median = MO3(arr1[l1], arr2[l2 + size2/2], arr2[l2 + size2/2 - 1]);
+      } else if (size1 % 2 == 0){
+        median = MO3(arr2[l2], arr1[l1 + size1/2], arr1[l1 + size1/2 - 1]);
+      }
+
+      // 2.3 l = odd
+      // makes difference if sm elem lies between
+      else if (size2 % 2 != 0 ) {
+        median = MO4(arr1[l1], arr2[l2 + size2/2 - 1], arr2[l2 + size2/2], arr2[l2 + size2/2 + 1]);
+      } else {
+        median = MO4(arr2[l2], arr1[l1 + size1/2 - 1], arr1[l1 + size1/2], arr1[l1 + size1/2 + 1]);
+      }
     }
-    // 2.2 l = even
+
+    // case 3: sm = 2
+    if (size1 == 2 || size2 == 2) {
+      // 3.1 l= 2
+      if (size1 == 2 && size2 == 2) {
+        median = MO4(arr1[l1], arr1[l1 + 1], arr2[l2], arr2[l2 + 1]);
+      }
+
+      // l = odd
+      else if (size1 % 2 != 0) {
+        median = MO3(arr1[l1 + size1/2],
+                Math.max(arr1[l1 + size1/2 - 1], arr2[l2]),
+                Math.min(arr1[l1 + size1/2 + 1], arr2[l2 + 1]));
+      } else if (size2 % 2 != 0) {
+        median = MO3(arr2[l2 + size2/2],
+                Math.max(arr2[l2 + size1/2 - 1], arr1[l1]),
+                Math.min(arr2[l2 + size2/2 + 1], arr1[l1 + 1]));
+      }
+
+      // l = even
+      else if (size1 % 2 == 0) {
+        median = MO4(arr1[l1 + size1/2], arr1[l1 + size1/2 - 1],
+                Math.max(arr1[l1 + size1/2 - 2], arr2[l2]),
+                Math.min(arr1[l1 + size1/2 + 1], arr2[l2 + 1]));
+      } else {
+        median = MO4(arr2[l2 + size2/2], arr2[l2 + size2/2 - 1],
+                Math.max(arr2[l2 + size2/2 - 2], arr1[l1]),
+                Math.min(arr2[l2 + size2/2 + 1], arr1[l1 + 1]));
+      }
+    }
+
     return median;
   }
 
-  private static float getMedian(int[] arr, int low, int high, int size) {
+  private static float MO3(int a, int b, int c) {
+    int min = Math.min(a, Math.min(b, c));
+    int max = Math.max(a, Math.max(b, c));
+
+    return (a + b + c) - max - min;
+  }
+
+  private static float MO4(int a, int b, int c, int d) {
+    int min = Math.min(a, Math.min(b, Math.min(c,d)));
+    int max = Math.max(a, Math.max(b, Math.max(c,d)));
+
+    return (float)(((a + b + c + d) - max - min)/2.0);
+  }
+
+  private static float getMedian(int[] arr, int low, int high) {
+    int size = high - low + 1;
     // check for size
     float median;
     if (size % 2 == 0) {
-      median = (float)((arr[size - 1] + arr[size])/2.0);
+      median = (float)((arr[low + size/2 - 1] + arr[low + size/2])/2.0);
     } else {
-      median = (float)(arr[size/2]);
+      median = (float)(arr[low + size/2]);
     }
     return median;
   }
