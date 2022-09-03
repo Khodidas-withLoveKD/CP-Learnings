af8e54667474bd6c71926553c81a335b19b5c4da
Khodidas_with_love_KD <chauhan.khodidas@gmail.com>
2022-09-03 21:40:34 +0530

    Did some Math Operations

---
+++ Algorithms/Python_Learnings/Math_Operations.py
@@ -0,0 +1,28 @@
+import math
+
+x = 2.9
+print(round(x))
+print(round(2.5))
+print(round(3.5))
+print(abs(-2.9))
+
+print(math.ceil(2.9))
+print(math.floor(2.9))
+
+print(math.comb(4, 2))
+print(math.fabs(-.4))
+print(math.factorial(4))
+
+print(1e100)
+print(-1e-100)
+print('gcd = ' + str(math.gcd(45,25, 75)))
+print('lcm = ' + str(math.lcm(45,25, 75)))
+print('isqrt = ' + str(math.isqrt(9)))
+
+print(math.modf(2.4))
+print('perm = ', math.perm(4,2))
+print('product of all nums = ', math.prod([1,2,3,4,5], start=2))
+print('remainder = ', math.remainder(20,3))
+
+print(1e3)
+print(1e-3)
