be4e42f5d858da4f4d05c79f2b138bfa9c68810e
Khodidas_with_love_KD <chauhan.khodidas@gmail.com>
2022-09-05 22:46:01 +0530

    Created Number Guessing Game

---
+++ Algorithms/Python_Learnings/Number_Guessing_Game.py
@@ -0,0 +1,28 @@
+'''
+    Guess the number between 0 to 9 (randomly generated)
+    Give them max 3 tries
+    Give them the warning messages like 'Too high/low'
+    Result Messages - You Won :), Oops! You Lost
+    Also, show them the no. of attempts left
+'''
+import random
+
+number_to_be_guessed = random.randint(0, 9)
+# print(f'Ans = {number_to_be_guessed}')
+ans_found = False
+attempts_left = 3
+
+while not ans_found and attempts_left:
+    print(f'---------\nAttempt: {3 - attempts_left + 1}')
+    user_guess = int(input('Enter your guess: '))
+    if user_guess < number_to_be_guessed:
+        print('Too Low')
+    elif user_guess > number_to_be_guessed:
+        print('Too High')
+    else:
+        print('Yaaayyy!!! You guessed it correctly :)')
+        ans_found = True
+    attempts_left -= 1
+
+if not ans_found:
+    print('Awwww :( Better luck next time Buddy')
