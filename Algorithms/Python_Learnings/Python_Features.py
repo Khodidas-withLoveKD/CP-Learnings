c2e51b665ed3d7df23a6e176cd53ab31952247ec
Khodidas_with_love_KD <chauhan.khodidas@gmail.com>
2022-09-08 10:39:02 +0530

    Unpacking

---
+++ Algorithms/Python_Learnings/Python_Features.py
@@ -0,0 +1,13 @@
+print('===== UNPACKING =====')
+coordinates = (1, 2, 3)
+# x = coordinates[0]
+# y = coordinates[1]
+# z = coordinates[2]
+x, y, z = coordinates
+# line 3, 4, 5 is exactly same as that of 6
+print(x, y, z)
+numbers = [1, 2, 3]
+# throws error
+# x, y = numbers
+print(x)
+print(y)
