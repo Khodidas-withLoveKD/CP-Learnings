ca71a6da9506ea807d72148b45ed2963434d1021
Khodidas_with_love_KD <chauhan.khodidas@gmail.com>
2022-09-08 21:23:41 +0530

    Learnt Funcitons by creating the get_emojis_in_input()

--- Algorithms/Python_Learnings/Emojis_Converter.py
+++ Algorithms/Python_Learnings/Emojis_Converter.py
@@ -1,23 +1,26 @@
-user_msg = input('Enter your message: ')
-words = user_msg.split(' ')
+def get_emojis_in_input(user_msg):
+    words = user_msg.split(' ')
 
-emojis_map = {
-    ':)': '😊',
-    ':(': '😞',
-    ':o': '😮',
-    '0:)': '😇'
-}
-output_msg = ''
-for word in words:
-    # === Way 3 ====
-    output_msg += emojis_map.get(word, word) + ' '
-    # === Way 1 ====
-    # if word in emojis_map:
-    #     output_msg += emojis_map[word]
-    # else:
-    #     output_msg += word
-    # === Way 2 ====
-    # output_msg += emojis_map[word] if word in emojis_map else word
-    # output_msg += ' '
+    emojis_map = {
+        ':)': '😊',
+        ':(': '😞',
+        ':o': '😮',
+        '0:)': '😇'
+    }
+    output_msg = ''
+    for word in words:
+        # === Way 3 ====
+        output_msg += emojis_map.get(word, word) + ' '
+        # === Way 1 ====
+        # if word in emojis_map:
+        #     output_msg += emojis_map[word]
+        # else:
+        #     output_msg += word
+        # === Way 2 ====
+        # output_msg += emojis_map[word] if word in emojis_map else word
+        # output_msg += ' '
+    return output_msg
 
-print(output_msg)
+
+user_input = input('Enter your message: ')
+print(get_emojis_in_input(user_input))
