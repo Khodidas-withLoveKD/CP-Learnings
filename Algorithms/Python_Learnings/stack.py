ddaf5878784ad6ff653c7394ab76133c713d3de2
Khodidas_with_love_KD <chauhan.khodidas@gmail.com>
2022-09-13 20:46:58 +0530

    stack using list

---
+++ Algorithms/Python_Learnings/stack.py
@@ -0,0 +1,11 @@
+print('==== USING LIST ====')
+stack = []
+stack.append(1)
+stack.append(2)
+stack.append(3)
+print(f'After inserting stack = {stack}')
+print(f'Pop(): {stack.pop()} | now stack = {stack}')
+print(f'Pop(): {stack.pop()} | now stack = {stack}')
+print(f'Pop(): {stack.pop()} | now stack = {stack}')
+# print(f'Pop(): {stack.pop()} | now stack = {stack}')
+
