9d207f34cc924b09a33a36600a975475ac3ad9b2
Khodidas_with_love_KD <chauhan.khodidas@gmail.com>
2022-10-05 21:51:03 +0530

    Sorting dictionaries;

---
+++ Algorithms/Python_Learnings/dictionaries/sorting_dictionaries.py
@@ -0,0 +1,13 @@
+di = {
+    'a': 5,
+    'c': 3,
+    'e': 1,
+    'b': 4,
+    'd': 2
+}
+
+print(di)
+print(f'Sort by keys = {sorted(di.keys())}')
+print(f'Sort by values = {sorted(di.values())}')
+print(f'Sort by items = {sorted(di.items())}')
+
