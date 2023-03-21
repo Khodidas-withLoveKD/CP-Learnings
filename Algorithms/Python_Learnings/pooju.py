d7f3c3ee33fa66c43053eb8bf2929afbbd2f86e0
Khodidas_with_love_KD <chauhan.khodidas@gmail.com>
2023-03-21 10:27:29 +0530

    Taught my sister Python Coding for the first time :)

---
+++ Algorithms/Python_Learnings/pooju.py
@@ -0,0 +1,59 @@
+# variable
+# name = "Pooju"
+# age = 27  # assignment
+#
+# # decision statements / conditional
+# if age < 20:
+#     print("Age is less than 20")
+#     print("POOJU")
+# elif age > 20 and age < 25:
+#     print("Age is between 20 and 25")
+# elif age == 26 or name == "Pooju":
+#     print("Name is Pooju")
+# else:
+#     print("Age is greater than 20")
+#     print("BHAILO")
+# print("OUTSIDE LOOP")
+
+
+# WHILE LOOP
+# i = 0
+# while i < 6:  # i = 5
+#     i += 1  # i = 4 + 1
+#     if i == 3:  # i = 5
+#         continue  # break
+#     print(i)  # 1, 2, 4, 5, 6
+# print("OUTSIDE LOOP")
+
+# # WHILE-ELSE LOOP
+# i = 0
+# while i < 5:
+#     print(i)  # i = 2
+#     i += 1    # 2 + 1
+#     # if i == 3:
+#     #     break
+# else:
+#     print("ELSE")
+
+
+# FOR LOOP
+# print("FOR Loop start")
+# for j in range(1, -10, -2):  # 0 -> 5 - 1
+#     print(j)
+
+
+# Question 1
+i = 0
+while i < 5:  # i = 3
+    print(i)  # 0, 1, 2
+    i += 1    # i = 2 + 1
+    if i == 3:  # i = 3
+        break
+else:
+    print(0)
+
+# Question 2
+# print((8+3)*100)  # BODMAS
+# # 8 + 3 = 11
+# # 11 * 100 = 1100
+# print("(8+3)*100")  # String so ans = (8+3)*100
