6476670a48912bb79bd8c75e1f7ee00d4626be6c
Khodidas_with_love_KD <chauhan.khodidas@gmail.com>
2023-07-25 22:49:16 +0530

    Learnt use of counters from collections'

---
+++ Algorithms/Python_Learnings/dictionaries/counters.py
@@ -0,0 +1,18 @@
+# learning counters
+from collections import Counter
+
+
+li = [1, 2, 3, 1, 3, 2, 1, 1, 5, 5, 5]
+counter = Counter(li)
+print(counter)
+print(f'counter[1] = {counter[1]}')
+print(f'counter.most_common(2) = {counter.most_common(2)}')
+li_to_be_added = [0, 0, 1]
+print(f'Counter before any change = {counter}')
+counter.update(li_to_be_added)
+print(f'After additon - Updated counter = {counter}')
+li_to_be_subtracted = [0, 0, 1]
+counter.subtract(li_to_be_subtracted)
+print(f'After subtraction - updated counter = {counter}')
+print(f'counter[100] = {counter[100]}')  # acts as a defaultdict
+
