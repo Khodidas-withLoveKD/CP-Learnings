7e42067f48dc65748d63ac9800f7cbebe50bc93e
Khodidas_with_love_KD <chauhan.khodidas@gmail.com>
2023-08-28 20:04:23 +0530

    Knapsack using bottom-up

--- Algorithms/Dynamic_Programming/0-1_knapsack_problem.py
+++ Algorithms/Dynamic_Programming/0-1_knapsack_problem.py
@@ -1,4 +1,4 @@
-def knapSack(W, wt, val, n):
+def knapSack_using_top_down(W, wt, val, n):
     # code here
     def add_item(i, curr_wt):
         if i >= n:
@@ -20,8 +20,27 @@ def knapSack(W, wt, val, n):
     return add_item(0, 0)
 
 
+def knapsack_using_bottom_up(W, wt, val, n):
+    # will do a bottom up approach
+    # so will require an array to store it
+    dp = [[0 for _ in range(W + 1)] for _ in range(N + 1)]
+    # initialized with 0 as the base case has 0
+    # so if there is 0 item or 0 weight the ans is 0
+    # start
+    for i in range(1, N+1):
+        for j in range(W+1):
+            item_included_value = 0
+            if j - wt[i-1] >= 0:
+                # max of weight inlcuding the item or excluding the item
+                item_included_value = val[i-1] + dp[i-1][j - wt[i-1]]  # current val + best val till the previous item and subtracting the current weight
+            item_excluded_value = dp[i-1][j]
+            dp[i][j] = max(item_included_value, item_excluded_value)
+
+    return dp[N][W]
+
 N = 3
 W = 4
 wt = [4, 5, 1]
 val = [1, 2, 3]
-print(knapSack(W, wt, val, N))
+print(knapSack_using_top_down(W, wt, val, N))
+print(knapsack_using_bottom_up(W, wt, val, N))
