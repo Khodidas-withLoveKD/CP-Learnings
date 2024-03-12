17a09717cc0aa59e4e8c2150bfde8e1c68e45eb0
Khodidas_with_love_KD <chauhan.khodidas@gmail.com>
2024-03-13 00:18:35 +0530

    Solved get_next_smallest_and_largest_num_using_binary with all the edge cases covered

--- Algorithms/Bitwise_Operations/next_smallest_largest_num.py
+++ Algorithms/Bitwise_Operations/next_smallest_largest_num.py
@@ -61,23 +61,26 @@ def get_next_smallest_and_largest_num_using_binary_str_manipulation(num):
 
 def get_next_nums_by_bit_manipulation(num):
     def get_zeros_index_and_ones_count(bin_num):
-        index, ones_count = len(bin_num), 0
+        index, zeros_count, ones_count = len(bin_num), 0, 0
         for i, bit in enumerate(bin_num[::-1]):
             if bit == '0':
+                zeros_count += 1
                 if ones_count > 0:
                     index = i
                     break
             else:
                 ones_count += 1
 
-        return index, ones_count
+        return index, zeros_count, ones_count
 
     def get_next_bigger_by_bit_manipulation(num):
         bin_num = bin(num)[2:]
         # print_bin_and_num(num, 'num')
         # find the rightmost zero which has atleast one 1 on right
-        index, ones_count = get_zeros_index_and_ones_count(bin_num)
+        index, zeros_count, ones_count = get_zeros_index_and_ones_count(bin_num)
         # print(f'index = {index} | ones_count = {ones_count}')
+        if ones_count == 0:
+            return 0
         # make the index as 1 to increase the num
         swap_mask = 1 << index
         # print_bin_and_num(swap_mask, 'swap_mask')
@@ -85,19 +88,27 @@ def get_next_nums_by_bit_manipulation(num):
         # print_bin_and_num(num, 'After swap num')
         # will keep the zeros to the leftmost and ones on the rightmost
         # to keep the num big and as close as possible to original num
+        # shift only the ones to someleft
+
+
         reset_mask = -1 << index
         # print_bin_and_num(reset_mask, 'reset_mask')
         num = num & reset_mask
-        # print_bin_and_num(num, 'After reset reset_mask')
-        if ones_count > 1:
-            ones_mask = 1 << (ones_count-2)
-            # print_bin_and_num(ones_mask, 'ones_mask')
-        # make the all the zeros
-        # num = num & reset_mask
-        # print_bin_and_num(num, 'After swap num')
-        # add all the ones_count - 1 nums to the right most
-            num = num | ones_mask
-            # print_bin_and_num(num, 'After adding ones_mask')
+        # print_bin_and_num(num, 'After reset num')
+        ones_mask = (1 << ones_count-1) - 1
+        # print_bin_and_num(ones_mask, 'ones_mask')
+        num = num | ones_mask
+        # print_bin_and_num(num, 'after ones_mask num')
+
+        # if ones_count > 1:
+        #     ones_mask = 1 << (ones_count-2)
+        #     print_bin_and_num(ones_mask, 'ones_mask')
+        # # make the all the zeros
+        # # num = num & reset_mask
+        #     print_bin_and_num(num, 'After swap num')
+        # # add all the ones_count - 1 nums to the right most
+        #     num = num | ones_mask
+        #     print_bin_and_num(num, 'After adding ones_mask')
 
         return num
 
@@ -121,6 +132,8 @@ def get_next_nums_by_bit_manipulation(num):
         # find the rightmost one which has at least one zero on right
         # find the index, and zeros_count
         index, zeros_count, ones_count = get_ones_index_and_zeros_count(bin_num)
+        if zeros_count == 0:
+            return -1
         # print(f'index = {index} | zeros_count = {zeros_count} | ones_count = {ones_count}')
         # swap the one with zero and reset the right bits
         # STEP 2: replace that one on index with 0 and reset
@@ -145,14 +158,13 @@ def get_next_nums_by_bit_manipulation(num):
     next_bigger = get_next_bigger_by_bit_manipulation(num)
     next_smaller = get_next_smaller_by_bit_manipulation(num)
     # print(f' num = {num} | bigger = {next_bigger} \n=====')
-    print(f'===========\nnum = {num} | Bigger = {next_bigger} | Smaller = {next_smaller} \n=========')
+    print(f'=====\nnum = {num} | Bigger = {next_bigger} | Smaller = {next_smaller} \n====')
 
-# TODO: get the edge cases
 testcases = [73, 5, 11, 2, 3, 15, 0, 1]
 greater = [74, 6, 13, 4, 5, 23, -1, 2]
 smaller = [70, 3, 7, 1, -1, -1, -1, -1]
 
-# get_next_nums_by_bit_manipulation(5)
+# get_next_nums_by_bit_manipulation(0)
 for num in testcases:
     get_next_nums_by_bit_manipulation(num)
 
