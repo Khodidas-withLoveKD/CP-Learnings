71f2536df1c88acf78907129a26557f044030bb3
k0c0pg6_Khodidas_KD <KHODIDAS.CHAUHAN@walmart.com>
2025-03-11 13:53:14 +0530

    Convert BST to sorted Linked List Intermediate | Completed CircularLinkedList:

---
+++ Algorithms/Linked_List/CircularLinkedList.java
@@ -0,0 +1,18 @@
+package Algorithms.Linked_List;
+
+public Class Main {
+
+public static void main(String[] args){
+    Node one = new Node(1);
+    Node two = new Node(2);
+    Node three = new Node(3);
+    one.next = two;
+    two.next = three;
+
+    Node head = one;
+    while (head != null) {
+        System.out.println(head.val);
+        head = head.next;
+    }
+}
+        }
